1b6d4f93bb01f822f2cee42bc79073a9009f1208
muhammad-huzaifah <huzaifah2009@gmail.com>
2021-01-21 16:27:43 +0700

    initial commit

--- geometri/segitiga.py
+++ geometri/segitiga.py
@@ -2,6 +2,6 @@ def info():
     return 'Modul menghitung rumus-rumus segitiga'
 
 
-def hitung_luas_segitiga (alas, tinggi):
+def hitung_luas_segitiga(alas, tinggi):
     luas_segitiga = alas * tinggi / 2
     return luas_segitiga
